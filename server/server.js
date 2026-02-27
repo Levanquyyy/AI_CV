d08b6242a57163987726ef6e45799781c06afb83
Yun <yun@Mac.lan>
2026-02-27 17:51:06 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
+import "dotenv/config";
 import "./config/instrument.js";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import "dotenv/config";
 import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controller/webhooks.js";
